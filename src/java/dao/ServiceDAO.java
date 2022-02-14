0a5d7a4735ac8e1ec62ef6aea7a624dcce39ded4
caotrangg <91126397+caotrangg@users.noreply.github.com>
2022-02-14 21:49:33 +0700

    trangct update view list service, view service detail
    hello

--- src/java/dao/ServiceDAO.java
+++ src/java/dao/ServiceDAO.java
@@ -1,14 +1,47 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright(C) 20022, FPT University
+ * CMS:
+ * Clinic Management System
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * 2022-02-08      1.0                 TrangCT          Service DAO
  */
 package dao;
 
+import entity.Pagination;
+import entity.Service;
+
 /**
- *
- * @author Thanh Tung
+ * <h1>Service DAO</h1>
+ * Interface ServiceDAO. Method access and return data. Its method is implements by ServiceDAOIpml class
+ * <p>
+ * 
+ * 
+ * @author TrangCT
+ * @version 1.0
+ * @since 2022-02-08
  */
 public interface ServiceDAO {
+  
+    /**
+     * Get all services
+     * @param pageIndex
+     * @param pageSize
+     * @return list services
+     */
+    Pagination<Service> getAllService(int pageIndex, int pageSize);
     
+    /**
+     * Count total service
+     * @return total service
+     */
+    int count();
+    
+    /**
+     * Get service by id
+     * @param id
+     * @return service
+     */
+    Service getById(int id);
 }
--- src/java/dao/impl/ServiceDAOImpl.java
+++ src/java/dao/impl/ServiceDAOImpl.java
@@ -1,17 +1,157 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright(C) 20022, FPT University
+ * CMS:
+ * Clinic Management System
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * 2022-02-08      1.0                 TrangCT          Service DAO Implement
  */
 package dao.impl;
 
 import context.DBContext;
 import dao.ServiceDAO;
+import entity.Pagination;
+import entity.Service;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
- * @author Thanh Tung
+ * <h1>Service DAO</h1>
+ * Data access object connect database and access data.
+ *   - count : count list services
+ *   - getAllService : get list services by paging
+ *   - getById : get service by id
+ * <p>
+ * 
+ * 
+ * @author TrangCT
+ * @version 1.0
+ * @since 2022-02-08
  */
 public class ServiceDAOImpl extends DBContext implements ServiceDAO {
-    
+
+    /**
+     * Logger for system
+     */
+    private static Logger logger = Logger.getLogger(UserDAOImpl.class.getName());
+
+    /**
+     * Method: Get All Service
+     * 
+     *      - get all services by pageIndex and pageSize
+     * 
+     * @param pageIndex integer
+     * @param pageSize integer
+     * @return pagination Pagination Service
+     */
+    public Pagination<Service> getAllService(int pageIndex, int pageSize) {
+        
+        Pagination<Service> pagination = new Pagination<>(); // pagination services
+
+        logger.log(Level.INFO, "getAllService");
+        Connection connecion = null; // connection database
+        PreparedStatement preparedStatement = null; 
+        ResultSet rs = null;
+        List<Service> services = new ArrayList<>();// list all services
+        try {
+            int totalItem = count(); // count total service
+            pagination.setCurrentPage(pageIndex);
+            pagination.setItemPerPage(pageSize);
+            pagination.setTotalItem(totalItem);
+
+            connecion = getConnection();
+            // Get data
+            preparedStatement = connecion.prepareStatement("select * from services ORDER BY service_id  OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
+            preparedStatement.setInt(1, pageIndex);
+            preparedStatement.setInt(2, pageSize);
+            //excute query
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) { 
+                Service service = new Service(); 
+                service.setServiceId(rs.getInt("service_id"));
+                service.setServiceName(rs.getString("service_name"));
+                service.setServiceBrief(rs.getString("service_brief"));
+                service.setServiceDescription(rs.getString("service_description"));
+                service.setServiceImage(rs.getString("service_image"));
+                services.add(service);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(preparedStatement);
+            closeConnection(connecion);
+        }
+        pagination.setData(services);// paging for list services
+        return pagination;
+    }
+
+    /**
+     * Method : count
+     *      - count total services in database
+     * @return an integer 
+     */
+    public int count() {
+        Connection connecion = null;
+        PreparedStatement countPreparedStatement = null;
+        ResultSet countResultSet = null;
+        try {
+            connecion = getConnection();
+            countPreparedStatement = connecion.prepareStatement("SELECT COUNT(service_id) AS id FROM services;");
+            countResultSet = countPreparedStatement.executeQuery();
+            if (countResultSet.next()) {
+                // get and return count total services
+                return countResultSet.getInt(1);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            closeResultSet(countResultSet);
+            closePreparedStatement(countPreparedStatement);
+            closeConnection(connecion);
+        }
+        return 0;
+    }
+
+    /**
+     * Method: Get Service By Id
+     *      - Get and return data of service by an id
+     * @param id
+     * @return service Service
+     */
+    public Service getById(int id) {
+        logger.log(Level.INFO, "getById");
+        Connection connecion = null; 
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null; 
+        Service service = new Service();
+        try {
+            connecion = getConnection();
+            // Get data
+            preparedStatement = connecion.prepareStatement("select * from services where service_id = ?");
+            preparedStatement.setInt(1, id);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                service.setServiceId(rs.getInt("service_id"));
+                service.setServiceName(rs.getString("service_name"));
+                service.setServiceBrief(rs.getString("service_brief"));
+                service.setServiceDescription(rs.getString("service_description"));
+                service.setServiceImage(rs.getString("service_image"));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(preparedStatement);
+            closeConnection(connecion);
+        }
+        return service;
+    }
+
 }
